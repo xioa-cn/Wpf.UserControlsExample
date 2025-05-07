7571719febe88dae4f00064bfafca27e5f4c2814
XIOA <1327916255@qq.com>
2025-05-07 17:28:29 +0800

    UseExampleAutoTable

---
+++ Wpf.UserControlsExample/ControlsExample/AnimationSeparator/UseExampleAutoTable.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using Wpf.UserControlsExample.ControlsExample.AnimationSeparator.ControlNoumenon;
+
+namespace Wpf.UserControlsExample.ControlsExample.AnimationSeparator;
+
+public partial class UseExampleAutoTable : UserControl
+{
+    private ObservableCollection<AnimatedSeparator> AnimatedSeparators { get; set; } =
+        new ObservableCollection<AnimatedSeparator>();
+
+    public UseExampleAutoTable()
+    {
+        this.SizeChanged += SizeChangedAni;
+
+        InitializeComponent();
+        SeparatorItemsControl.ItemsSource = AnimatedSeparators;
+    }
+
+    private void SizeChangedAni(object sender, SizeChangedEventArgs e)
+    {
+        var normalMargin = 0;
+    }
+}
