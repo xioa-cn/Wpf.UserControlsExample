ec9839dc0e4df4a8ee5bf50357dafcf724b422f6
XIOA <1327916255@qq.com>
2025-05-07 13:38:25 +0800

    Slider

---
+++ Wpf.UserControlsExample/ControlsExample/ColoredAnimationSlider/ControlNoumenon/ColoredAnimationSlider.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Wpf.UserControlsExample.ControlsExample.ColoredAnimationSlider.ControlNoumenon;
+
+public partial class ColoredAnimationSlider : UserControl
+{
+    public ColoredAnimationSlider()
+    {
+        InitializeComponent();
+    }
+}
