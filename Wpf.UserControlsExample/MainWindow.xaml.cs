90f54a2bb5e11bb5450e9ddcbe267098a974831e
XIOA <1327916255@qq.com>
2025-05-08 15:51:20 +0800

    initPage

--- Wpf.UserControlsExample/MainWindow.xaml.cs
+++ Wpf.UserControlsExample/MainWindow.xaml.cs
@@ -9,10 +9,23 @@ namespace Wpf.UserControlsExample;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private readonly MainViewModel mainViewModel;
     public MainWindow()
     {
-        this.DataContext = new MainViewModel();
+        mainViewModel = new MainViewModel();
+        this.DataContext = mainViewModel;
         InitializeComponent();
+        this.Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        dynamic item = mainViewModel.Content[0];
+        if (item != null)
+            if (item.e is Type t)
+            {
+                GoView(t);
+            }
     }
 
     private Dictionary<string, object?> _data = new Dictionary<string, object?>();
@@ -20,6 +33,11 @@ public partial class MainWindow : Window
     private void ContentGoonView(object sender, RoutedEventArgs e)
     {
         if ((sender as Button)?.Tag is not Type t) return;
+        GoView(t);
+    }
+
+    private void GoView(Type t)
+    {
         if (t.FullName != null && _data.TryGetValue(t.FullName, out var value))
         {
             this.ContentControl.Content = value;
